14677300386fd0c4095f513ba79018a88d86ac6b
km-saifullah <kmsaifullah16@gmail.com>
2024-08-12 22:14:00 +0600

    quick-response package work done

---
+++ build.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+const { exec } = require("child_process");
+
+// Create the dist directory if it doesn't exist
+const distDir = path.join(__dirname, "dist");
+if (!fs.existsSync(distDir)) {
+  fs.mkdirSync(distDir);
+}
+
+// Copy the file for CommonJS
+fs.copyFileSync(
+  path.join(__dirname, "src/index.js"),
+  path.join(distDir, "index.cjs.js")
+);
+
+// Convert the file to ES Module
+const esmContent = fs
+  .readFileSync(path.join(__dirname, "src/index.js"), "utf8")
+  .replace(/module\.exports\s*=\s*/, "export default ");
+
+fs.writeFileSync(path.join(distDir, "index.esm.js"), esmContent);
+
+// Run TypeScript declaration file generation (if needed)
+// exec('tsc --declaration --emitDeclarationOnly --outDir dist', (err, stdout, stderr) => {
+//   if (err) {
+//     console.error(`Error generating type declarations: ${stderr}`);
+//   } else {
+//     console.log('Type declarations generated successfully.');
+//   }
+// });
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const apiResponse = (statusCode, message, data = null) => {
+  return {
+    statusCode,
+    message,
+    data,
+    timestamp: new Date().toDateString(),
+  };
+};
+
+export default apiResponse;
+module.exports = apiResponse;
